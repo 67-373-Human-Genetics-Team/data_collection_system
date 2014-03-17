2e5591df4a1b925360fefc1e97d21b8426c7687f
Min Allison <amin1@andrew.cmu.edu>
2014-03-17 00:40:42 -0400

    Updated admin routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var controllers = {
 	survey : require('./controllers/survey'),
-	admin : require('./controllers/admin')
+	admin : require('./controllers/admin'),
+	api : require('./controllers/api')
 };
 var app = module.exports = express.createServer();
 
@@ -18,8 +19,9 @@ app.configure(function(){
 
 // Routes
 app.get('/', controllers.survey.home);
-app.get('/surveys', controllers.survey.list);
-app.get('/suveys/new', controller.survey.post);
+app.get('/admin/surveys', controllers.admin.list);
+app.get('/admin/survey/new', controllers.admin.newsurvey);
+app.post('/api/new', controllers.api.post);
 
 app.listen(3000);
 console.log("Express server listening on port 3000");
