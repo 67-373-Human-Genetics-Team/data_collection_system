b4754f0af3e72cf1748e3767a4cfb6b32e7cdfee
Min Allison <amin1@andrew.cmu.edu>
2014-03-19 00:32:25 -0400

    Working on survey views

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.configure(function(){
   app.use(app.router);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
 });
 
 // Routes
