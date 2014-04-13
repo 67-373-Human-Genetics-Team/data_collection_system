91155f4c47a4bb0af43c0fef197f9c7c0d6ab2d4
Min Allison <amin1@andrew.cmu.edu>
2014-04-13 19:49:49 -0400

    Added joins to models using mongoose population

--- models/response.js
+++ models/response.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
-	ObjectId = Schema.ObjectId;
+	ObjectId = Schema.ObjectId,
+    Survey = require('./survey.js').schema,
+    Participant = require('./participant.js').schema;
 
 var ResponseSchema = new Schema({
-	survey_id: ObjectId,
-	participant_id: ObjectId,
-	date: {type: Date, default: Date.now},
-	answers: [String]
+	survey_id: { type: ObjectId, ref: 'Survey' },
+	participant_id: { type: ObjectId, ref: 'Participant' },
+	date: { type: Date, default: Date.now },
+	answers: [ String ]
 });
 
 module.exports = mongoose.model('Response', ResponseSchema);
--- models/survey.js
+++ models/survey.js
@@ -1,16 +1,17 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
-	ObjectId = Schema.ObjectId
-    Question = require('./question.js').schema;
+	ObjectId = Schema.ObjectId,
+    Question = require('./question.js').schema,
+    Response = require('./response.js').schema;
 
 var SurveySchema = new Schema({
 	name: String,
 	status: String,
-    created_at: {type: Date, default: Date.now},
+    created_at: { type: Date, default: Date.now },
     start_date: Date,
     end_date: Date,
-	questions: [Question],
-	responses: [ObjectId]
+	questions: [ Question ],
+	responses: [{ type: ObjectId, ref: 'Response' }]
 });
 
 module.exports = mongoose.model('Survey', SurveySchema);
