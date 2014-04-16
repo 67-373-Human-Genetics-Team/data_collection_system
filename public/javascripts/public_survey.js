7ca158767b8660c54d60c8c7e409cde3a0c90a85
Min Allison <amin1@andrew.cmu.edu>
2014-04-16 00:01:50 -0400

    Populates publications by participant's name - used hardcoded example

--- public/javascripts/public_survey.js
+++ public/javascripts/public_survey.js
@@ -1,9 +1,121 @@
 $(function() {
+
     // Get participant id from URL pathname and update value
     var participant_id = window.location.pathname.split( '/' ).pop();
     $("#participant_id").val(participant_id);
+
+    // Get participant firstname lastname and call publications api
+    getParticipant(participant_id);
+
     $("#participant-survey-form").submit(postResponse);
+    
 });
+
+// Demo publication data, will remove after server setup
+var publications = [
+    {
+      "title": "The implications of ENCODE for diagnostics.",
+      "journal": "Some Journal",
+      "pub-date": "mm/dd/yyyy",
+      "authors": [
+        {"name": "Firstname Lastname1",
+         "organization": "division name, organization name, address"
+        },
+        {"name": "Firstname Lastname2",
+         "organization": "division name, organization name, address"
+        },
+        {"name": "Firstname Lastname3",
+         "organization": "division name, organization name, address"
+        },
+        {"name": "Firstname Lastname4",
+         "organization": "division name, organization name, address"
+        },
+      ],
+      "data-source": "pmc",
+      "id": "123456789"
+    },
+    {
+      "title": "Why life science needs its own Silicon Valley.",
+      "journal": "Harvard Business Review",
+      "pub-date": "mm/dd/yyyy",
+      "authors": [
+        {"name": "Firstname Lastname1",
+         "organization": "division name, organization name, address"
+        },
+        {"name": "Firstname Lastname2",
+         "organization": "division name, organization name, address"
+        },
+        {"name": "Firstname Lastname3",
+         "organization": "division name, organization name, address"
+        },
+        {"name": "Firstname Lastname4",
+         "organization": "division name, organization name, address"
+        },
+      ],
+      "data-source": "pmc",
+      "id": "123456789"
+    },
+]
+
+// Gets participant's name and calls getPublications(name)
+function getParticipant(id) {
+    $.ajax('/api/participants/'+id, {
+        cache: false,
+        type: 'GET',
+        success: function(data) {
+            var participant = data;
+            var participantName = participant.first_name+' '+participant.last_name;
+            console.log('Participant: '+participantName);
+            // Demo with Dietrich Stephan search
+            getPublications(participantName);
+        }
+    });
+};
+
+// Gets publications from name and displays list
+function getPublications(name) {
+    console.log('Function getPublications: '+name);
+
+    $("#participant-survey-form").hide();
+    $("#publications-box").fadeIn('slow');
+
+    $('#publications-box').append("<form id='publications-form'></form>");
+
+    for (var i=0; i<publications.length; i++) {
+        $('#publications-form').append("<div><input class='publication' type='checkbox' onclick='toggle();' value='"+publications[i].title+"'>"+publications[i].title+" ("+publications[i]['pub-date']+")"+"</input></div>")
+    }
+
+    $('#publications-form').append("<button type='button' onclick='populatePublicationDropdown();'>Confirm</button>");
+   // $.ajax('/publications', {
+   //      cache: false,
+   //      type: 'POST',
+   //      data: {
+   //          name: name
+   //      },
+   //      success: function(data) {
+   //          var publications = data;
+   //          // update divs with publication titles
+   //      }
+   //  }); 
+};
+
+// Remove this eventually. Using it to see what is checked.
+function toggle() {
+    var chosenPublications = [];
+    $('#publications-form input:checkbox:checked').each(function() {
+        chosenPublications.push($(this).val());
+    });
+    console.log(chosenPublications);
+};
+
+// Populates the publications dropdown list based on checked boxes
+function populatePublicationDropdown() {
+    $("#publications-box").slideUp('slow');
+    $("#participant-survey-form").fadeIn('slow');
+    $('#publications-form input:checkbox:checked').each(function() {
+        $("#publication-dropdown").append("<option value='"+$(this).val()+"'>"+$(this).val()+"</option>")
+    });
+};
 
 // Submitting response to survey
 function postResponse() {
