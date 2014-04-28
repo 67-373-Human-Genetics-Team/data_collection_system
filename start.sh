f14440778ba842069163e61711296923c181e49d
Min Allison <allisonmin@gmail.com>
2014-04-28 10:36:26 -0400

    fixed start script

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
-# !/bin/bash
+#!/bin/bash
 # start.sh
 # This starts the application with logging to mongo.log and node.log
 
+# Handles hangup, ctrl+c, terminate, and ctrl+\
+trap 'mongo --eval "use admin; db.shutdownServer();"' 1 2 3 15
 
 # Start database server
 mkdir -p db
-mongod --dbpath ./db > mongo- `date +%F-%H-%M-%S`.log 2>&1
+mongod --fork --dbpath ./db --logpath ./mongod-`date +%F-%H-%M-%S`.log
 
 # Start node server
 node app.js > node-`date +%F-%H-%M-%S`.log 2>&1
